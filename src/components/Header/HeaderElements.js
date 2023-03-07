4ca93dc2d3daae487f4448150cfa4dba896f34f3
gammbol <71922180+gammbol@users.noreply.github.com>
2023-03-07 20:29:46 +0300

    finished mobile menu

--- src/components/Header/HeaderElements.js
+++ src/components/Header/HeaderElements.js
@@ -81,3 +81,61 @@ export const MobileWrapper = styled.div`
     display: block;
   }
 `
+
+export const MobileMenuContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #000;
+  width: 100%;
+  height: 100%;
+  padding: 35px 5px;
+  z-index: 4;
+  display: flex;
+  flex-direction: column;
+  opacity: ${({displayed}) => (displayed ? '1' : '0')};
+  animation: ${({displayed}) => (displayed ? 'Appearance .9s forwards' : 'Disappearance .9s forwards')};
+  transition: .9s all ease-in-out;
+
+  @keyframes Appearance {
+    from {
+      transform: translate(-100vh);
+    }
+    to {
+      transform: translate(0);
+    }
+  }
+  @keyframes Disappearance {
+    to {
+      transform: translate(-100vh);
+    }
+  }
+`
+
+export const MobileNavMenu = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+export const MobileNavItem = styled.div`
+  box-sizing: content-box;
+  width: 40%;
+  padding: 10px 0;
+  height: 5%;
+  color: #fff;
+  border-radius: 50px;
+  transition: .3s ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+
+
+  &:hover {
+    background: #01bf71;
+    color: #000;
+  }
+`
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Reu from '../../images/rea-logo.svg'
 import {
 	HeaderContainer,
 	HeaderWrapper,
 	Logo,
 	LogoContainer,
+	MobileMenuContainer,
+	MobileNavItem,
+	MobileNavMenu,
 	MobileWrapper,
 	NavItem,
 	NavLinks,
 	NavMenu
 } from "./HeaderElements";
 import {GiHamburgerMenu} from "react-icons/gi";
+import {AiOutlineClose} from "react-icons/ai";
 
 function Header(props) {
+	const [state, setState] = useState();
+	const MobileMenuListener = () => {
+		setState(!state);
+	}
+
 	return (
 		<HeaderContainer>
 			<HeaderWrapper>
 				<LogoContainer>
 					<Logo src={Reu}/>
 				</LogoContainer>
-				<MobileWrapper>
-					<GiHamburgerMenu className={'burgerMenu'} style={{width: '100%', height: '100%'}}/>
+				<MobileWrapper onClick={MobileMenuListener}>
+					<GiHamburgerMenu style={{width: '100%', height: '100%'}}/>
 				</MobileWrapper>
 				<NavMenu>
 					<NavItem>
@@ -37,6 +46,25 @@ function Header(props) {
 					</NavItem>
 				</NavMenu>
 			</HeaderWrapper>
+			<MobileMenuContainer displayed={state}>
+				<MobileWrapper style={{marginLeft: 'auto'}}>
+					<AiOutlineClose style={{width: '35px', height: '35px', zIndex: '5'}} onClick={MobileMenuListener}/>
+				</MobileWrapper>
+				<MobileNavMenu>
+					<MobileNavItem>
+						<NavLinks to="#">О нас</NavLinks>
+					</MobileNavItem>
+					<MobileNavItem>
+						<NavLinks to="#">О нас</NavLinks>
+					</MobileNavItem>
+					<MobileNavItem>
+						<NavLinks to="#">О нас</NavLinks>
+					</MobileNavItem>
+					<MobileNavItem>
+						<NavLinks to="#">О нас</NavLinks>
+					</MobileNavItem>
+				</MobileNavMenu>
+			</MobileMenuContainer>
 		</HeaderContainer>
 	);
 }
